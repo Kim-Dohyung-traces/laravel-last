c934f94fc0b1f5275031e0a63989c61d811671ec
정인식 <insick6763@gmail.com>
2019-12-03 19:58:30 +0900

    members myFix

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Validator;
 
 class MembersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $members = \App\Members::get();
@@ -21,30 +16,15 @@ class MembersController extends Controller
     
     public function ajaxIndex(Request $request, $id){
 
-        // print($id);
         $members = \App\Members::where('id',$id)->first();
-        // $members = \App\Members::get();
         
         return response()->json($members,201);
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        $members = new \App\Members;
-
-        return view('members.create',compact('members'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {   
         // dd($request->file);
@@ -64,37 +44,15 @@ class MembersController extends Controller
         return response()->json($members,201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(\App\Members $member)
     {
-        print("여기는 쇼");
-        return view("members.show",compact("member"));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(\App\Members $member)
     {
-        return view('members.edit',compact('member'));
+        # return view('members.edit',compact('member'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // \App\Http\Requests\MembersRequest $request, \App\Members $member
     public function update(Request $request, $id)
     {   
         $validator = Validator::make($request->all(), [
@@ -109,20 +67,10 @@ class MembersController extends Controller
         $members = \App\Members::where('id',$id)->first();
 
         return response()->json($members,201);
-        // return response()->json([],204);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(\App\Members $member)
     {
-        // print("haha");
-        // $board = \App\Board::Class;
-        // $idd = $board->find($id);
         $member->delete();
     
         return response()->json([],204);
