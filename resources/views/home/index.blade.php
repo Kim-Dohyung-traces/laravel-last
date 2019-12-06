9fe71ec0264e4eee58fabe7fb95ed6afc11c58d8
notebook <btrya23@gmail.com>
2019-12-06 10:50:54 +0900

    home branch3

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,17 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app') -->
 
 @section('content')
   <!-- Page Header -->
   <header class="masthead">
     <div class="page-header">
       <h2>홈페이지</h2>
-      <div class="home-container">
-        <div class="home-img">
-          <img src="http://127.0.0.1:8000/img/home-bg.jpg" alt="Background Image">
+      <div>
+        <div>
+          <img style="width:100%;" src="http://127.0.0.1:8000/img/home-bg.jpg" alt="Background Image">
         </div>
-        <div class="home-sessions">
+        <div>
           @if (Auth::guest())
-          <form class="home-sessions-form" action="{{ route('sessions.store') }}" method="POST" role="form" class="form__auth">
+          <form style="border: 1px solid black" action="{{ route('sessions.store') }}" method="POST" role="form" class="form__auth">
             {!! csrf_field() !!}
             <div>
               <input type="email" name="email"  placeholder="이메일" value="admin@mail.com" autofocus />
@@ -23,11 +23,11 @@
               <button class="btn" type="submit">
                 로그인
               </button>
-              <a href="{{ route('users.create') }}">회원가입</a>
+              <a class="btn" href="{{ route('users.create') }}">회원가입</a>
             </div>
           </form>
           @else
-          <form class="home-sessions-form" style="display: inline-block;" action="{{ route('sessions.destroy') }}" method="GET" role="form" class="form__auth">
+          <form style="border: 1px solid black" action="{{ route('sessions.destroy') }}" method="GET" role="form" class="form__auth">
             {!! csrf_field() !!}
             <div>
               <label for="">이메일 : </label>
