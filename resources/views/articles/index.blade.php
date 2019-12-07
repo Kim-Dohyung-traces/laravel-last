2a1cdaee8547a58fdea463d97783a5e4dbbd075b
notebook <btrya23@gmail.com>
2019-12-08 03:44:34 +0900

    tag error fix

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 @php $viewName = 'articles.index'; @endphp
-
 <div class="page-header">
     <h4>
         <a href="{{ route('articles.index') }}">
@@ -19,6 +18,7 @@
         새 글 쓰기
 </div>
 
+<div class="test">
 <div class="row container__article">
     <div class="col-md-3 sidebar__article">
         <aside>
@@ -48,10 +48,10 @@
         @endif
     </div>
 </div>
+</div>
 @stop
 @section('script')
 <script>
-
 //게시글 눌렀을 경우의 ajax참고 ----------------------
 var article_id = null;                  //article_id
     //게시글 열고 닫고의 토글기능을 위한 변수
@@ -61,8 +61,6 @@ for(var i = 1; i < count+1; i++){
     article[i] = 0;
 }
 //-----------------------------------------------
-
-
 //새글쓰기 버튼
 var count = 0;
 $(document).on('click', '.btn__create__article', function(e) {
@@ -78,7 +76,6 @@ $(document).on('click', '.btn__create__article', function(e) {
     el_create.toggle('fast').focus();
     $('.btn__create__article').html(text);
 });
-
 //게시글 작성
 $(document).on('click', '.btn__save__article', function(e) {
     var form = $('#article_create_form')[0];
@@ -98,7 +95,6 @@ $(document).on('click', '.btn__save__article', function(e) {
         el_create.toggle('fast').focus();
     });
 });
-
 //선택한 태그인 게시글만 보여줌
 $(document).on('click', '.btn__tag__article', function(e) {
     var tag = $(this).closest('.btn__tag__article').data('id');
@@ -106,7 +102,7 @@ $(document).on('click', '.btn__tag__article', function(e) {
         type: 'GET',
         url: `tags/${tag}/articles`,
     }).then(function (data){
-        $('.container__article').load(`tags/${tag}/articles .container__article`);
+        $('.test').load(`tags/${tag}/articles .container__article`);
     });
 });
 //게시글 눌렀을 경우
@@ -125,7 +121,6 @@ $(document).on('click', '.btn__show__article', function(e) {
         }
     });
 });
-
 //글 목록 버튼
 $(document).on('click', '.button__list__articles', function(e) {
     $.ajax({
@@ -136,7 +131,6 @@ $(document).on('click', '.button__list__articles', function(e) {
         $('#main_container').load(`/articles #main_container`);
     });
 });
-
 //게시글 수정 버튼
 $(document).on('click', '.button__edit__articles', function(e) {
     $.ajax({
@@ -146,7 +140,6 @@ $(document).on('click', '.button__edit__articles', function(e) {
         $(`.media${article_id}`).load(`/articles/${article_id}/edit #main_container`);
     });
 });
-
 //게시글 수정 완료 버튼
 $(document).on('click', '.button__update__articles', function(e) {
     var form = $(`#article_edit_form1`)[0];
@@ -167,7 +160,6 @@ $(document).on('click', '.button__update__articles', function(e) {
 });
 //게시글 삭제 버튼
 $(document).on('click', '.button__delete__articles', function(e) {
-
     if (confirm('글을 삭제합니다.')) { //글을 삭제합니다 경고창에서 yes를누르면 true
         $.ajax({
             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},  
@@ -179,7 +171,6 @@ $(document).on('click', '.button__delete__articles', function(e) {
         });
     }
 });
-
 //댓글 생성
 $(document).on('click', '.btn__create__comment', function(e) {
     var parent_id =  $(this).closest('.item__comment').data('id');  //대댓글이면 부모 댓글id, 아니면 null
@@ -234,7 +225,6 @@ var commentId = $(this).closest('.item__comment').data('id');
         });
     }
 });
-
 //답글쓰기버튼 textarea toggle
 $(document).on('click', '.btn__reply__comment', function(e) {
     var el__create = $(this).closest('.item__comment').find('.media__create__comment').first(),
@@ -242,7 +232,6 @@ $(document).on('click', '.btn__reply__comment', function(e) {
     el__edit.hide('fast');
     el__create.toggle('fast').end().find('textarea').focus();
 });
-
 //댓글 수정버튼 textarea toggle
 $(document).on('click', '.btn__edit__comment', function(e) {
     var el__create = $(this).closest('.item__comment').find('.media__create__comment').first(),
@@ -250,7 +239,6 @@ $(document).on('click', '.btn__edit__comment', function(e) {
     el__create.hide('fast'); // 답글쓰기의 전송하기 숨김
     el__edit.toggle('fast').end().find('textarea').first().focus();
 });
-
 //좋아요 기능
 $(document).on('click', '.btn__vote__comment', function(e) {
     var self = $(this),
